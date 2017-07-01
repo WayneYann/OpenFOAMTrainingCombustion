2064c2ec29f010767b73dbd9961dfc942ec25cef
Alberto Cuoci <alberto.cuoci@polimi.it>
2017-07-01 12:44:44 +0200

    Added preProcessAll.sh utility to preprocess every mech

---
+++ PreProcessing/preProcessAll.sh
@@ -0,0 +1,23 @@
+cd Global_H2_1step
+OpenSMOKEpp_CHEMKIN_PreProcessor
+cd ..
+
+cd GRI30
+OpenSMOKEpp_CHEMKIN_PreProcessor
+cd ..
+
+cd POLIMI_H2_1412
+OpenSMOKEpp_CHEMKIN_PreProcessor
+cd ..
+
+cd POLIMI_H2CO_1412
+OpenSMOKEpp_CHEMKIN_PreProcessor
+cd ..
+
+cd POLIMI_H2CO_NOX_1412
+OpenSMOKEpp_CHEMKIN_PreProcessor
+cd ..
+
+cd POLIMI_H2_NOX_1412
+OpenSMOKEpp_CHEMKIN_PreProcessor
+cd ..
